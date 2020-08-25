630b79ed61d88fb3edb4c2264ba681daa82898c9
effysetumeni87 <effysetumeni87@gmail.com>
2020-08-26 01:17:06 +0200

    UPDATE

---
+++ src/components/about/firstSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <q-card flat class="row justify-center">
+          <q-card-section class="text-weight-bold text-center">
+              About Us
+          </q-card-section>
+          <q-card-section>
+              <div>
+                  The Economic Governance Initiative (EGI) is a five-partnership consortium led by Udugu Institute to pilot approaches for greater engagement between Civil Society Organizations (CSOs), Government, and Private Sector in order to improve economic governance. EGI uses an Issue-Based Programming (IBP) approach in order to maintain relevance and responsiveness in addressing policies within a changing socio-economic and governance changing landscape. EGI also uses evidence-based Policy Dialogue on issues of Youth Empowerment, Job Creation, mutual Transparency and accountability, sustainable budget management, and innovation in the way systems and institutions develop to use new technologies to foster greater economic governance. EGI will strengthen economic governance in Zimbabwe by promoting greater responsiveness on the part of Local Authorities and government departments to Citizens demands, resulting in higher growth in incomes and employment at the level of enterprises and SMEs. It is underpinned by the need to leverage on the Transitional Stabilization Program (TSP) and support improved economic governance through a framework informed by Devolved Governance as provided for in the 2013 Zimbabwe Constitution; and which in turn will contribute to strategies for the 2021-2025 National Development Plan.
+              </div>
+          </q-card-section>
+      </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/about/secondSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+     <q-card flat class="row justify-center">
+          <q-card-section class="text-weight-bold text-center">
+              EGI PARTNERS
+          </q-card-section>
+          <q-card-section class="col-12">
+            <q-card bordered class="row">
+              <div class="col-3 bg-grey-1">
+                  <q-img src="statics/pl1.png" style="width:150px; height:150px"/>
+              </div>
+              <q-card-section class="col-8">
+                  2
+              </q-card-section>
+              <div class="col-1">
+                  3
+              </div>
+            </q-card>
+          </q-card-section>
+     </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
